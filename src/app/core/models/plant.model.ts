e4a2aa5222ccb92d16f63b22ea2136fbc6e268ea
IDS-HUGO <hugofranciscoluis@gmail.com>
2024-10-04 10:30:24 -0600

    Updates

--- src/app/core/models/plant.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Planta {
-    idPlanta: number;
-    nombreCientifico: string;
-    nombreComun: string;
-    frecuenciaRiego: string;
-    condicionesLuz: string;
-    fechaRegistro: Date;
-  }
-  
--- src/app/core/models/user.model.ts
+++
@@ -1,7 +0,0 @@
-export interface User {
-    id: number;
-    nombre: string;
-    email: string;
-    contrasena: string;
-  }
-  
